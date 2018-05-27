b6b7d555263733d0f967be872315e6fdad413bf8
ashley nordan <abuxbaum@mcad.edu>
2018-05-27 13:36:01 -0500

    initial beggining

---
+++ house/script.js
@@ -0,0 +1,108 @@
+  // textillate to enhance the h1's on each page
+  $(document).ready(function() {
+	  console.log("javascriptloaded")
+	  
+	    $(".fancy_title").textillate();
+	  });
+	  
+	  
+	 // for learn page hiding content under questions
+
+ $(".readLight").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	  $(".lightContent").hide();
+	  
+	  $(".readLight").click(function() {
+		  console.log("clicky")
+    $(".lightContent").fadeToggle("slow");
+  });
+  	 
+	  $(".readDirection").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	  $(".directionContent").hide();
+	  
+	  $(".readDirection").click(function() {
+		  console.log("clicky")
+    $(".directionContent").fadeToggle("slow");
+  });
+  
+   $(".readLong").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	  $(".longContent").hide();
+	  
+	  $(".readLong").click(function() {
+		  console.log("clicky")
+    $(".longContent").fadeToggle("slow");
+  });
+  
+  // for spotlight page hiding info under each plant
+  
+     $(".readAloe").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	  $(".aloeContent").hide();
+	  
+	  $(".readAloe").click(function() {
+		  console.log("clicky")
+    $(".aloeContent").fadeToggle("slow");
+  });
+  
+     $(".readPothos").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	  $(".pothosContent").hide();
+	  
+	  $(".readPothos").click(function() {
+		  console.log("clicky")
+    $(".pothosContent").fadeToggle("slow");
+  });
+  
+  
+     $(".readCactus").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	  $(".cactusContent").hide();
+	  
+	  $(".readCactus").click(function() {
+		  console.log("clicky")
+    $(".cactusContent").fadeToggle("slow");
+  });
+  
+  // for faq page to hide content under each questions
+  
+       $(".readWilting").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	  $(".wiltingContent").hide();
+	  
+	  $(".readWilting").click(function() {
+		  console.log("clicky")
+    $(".wiltingContent").fadeToggle("slow");
+  });
+  
+       $(".readYellow").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	  $(".yellowContent").hide();
+	  
+	  $(".readYellow").click(function() {
+		  console.log("clicky")
+    $(".yellowContent").fadeToggle("slow");
+  });
+  
+       $(".readBurnt").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	  $(".burntContent").hide();
+	  
+	  $(".readBurnt").click(function() {
+		  console.log("clicky")
+    $(".burntContent").fadeToggle("slow");
+  });
+  
+       $(".readBrown").fadeTo(1000, 0.7).fadeTo(800, 0.3).fadeTo(600, 1.0);
+	  
+	  $(".brownContent").hide();
+	  
+	  $(".readBrown").click(function() {
+		  console.log("clicky")
+    $(".brownContent").fadeToggle("slow");
+  });
+  
+  
+  
+  
